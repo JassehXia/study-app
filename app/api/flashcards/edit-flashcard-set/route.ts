4beb35f328820ac5dbb26dbf29fafa3da969cee6
JassehXia <jassehxia@gmail.com>
2025-12-02 10:01:56 -0500

    feat: implement PATCH route for editing flashcard sets with error handling

--- app/api/flashcards/edit-flashcard-set/route.ts
+++ app/api/flashcards/edit-flashcard-set/route.ts
@@ -1,6 +1,27 @@
-import Prisma from '@prisma/client';
+import prisma from '@/lib/db';
 import { NextResponse } from 'next/server';
-export default async function POST(req: Request) {
+export async function PATCH(req: Request) {
+    try {
+        const body = await req.json();
+        const { id, title, description } = body;
 
+        if (!id) {
+            return NextResponse.json({ error: "Missing flashcard ID" }, { status: 400 })
+        }
+
+        const updated = await prisma.flashcardSet.update({
+            where: { id },
+            data: {
+                title,
+                description
+            }
+        })
+
+        return NextResponse.json(updated, { status: 200 });
+
+    } catch (err) {
+        console.error("Updated failed, ", err);
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
+    }
 
 }
---
+++ app/api/flashcards/edit-flashcard/route.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/db";
+
+export default function POST(req: Request) {
+
+}
